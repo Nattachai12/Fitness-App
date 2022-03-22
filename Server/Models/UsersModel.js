d85a67c9f1e3b2654d1f4bde1e18da6a4d45f7ee
Nattachai12 <nattachainudol@gmail.com>
2022-03-22 11:29:58 -0700

    Add .env file

--- Server/Models/UsersModel.js
+++ Server/Models/UsersModel.js
@@ -1,7 +1,10 @@
 const { Pool } = require('pg');
-
-const PG_URI =
-  "postgres://tcqbdxxd:vK6TGZQupL5f0ZqfjvFIbJA2oIqBaxPH@kashin.db.elephantsql.com/tcqbdxxd";
+console.log(`Your port is ${process.env.PG_URI}`); // undefined
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your port is ${process.env.PG_URI}`); // 8626
+// const PG_URI =
+//   "postgres://tcqbdxxd:vK6TGZQupL5f0ZqfjvFIbJA2oIqBaxPH@kashin.db.elephantsql.com/tcqbdxxd";
 
 const pool = new Pool({
   connectionString: PG_URI,
